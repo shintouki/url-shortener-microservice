9cc94c77f0eca28ff8843c4bc9782c9957159afe
Ted Shin <tdyshin@gmail.com>
2016-07-24 05:28:08 +0000

    Add html

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+  
+});
+
 app.get("/", function (req, res) {
    
    res.send("url shortener");
